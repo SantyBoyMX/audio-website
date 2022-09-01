a97e07416df09d47d91a0532193a1784b4e063b7
SantyBoyMX <97777706+SantyBoyMX@users.noreply.github.com>
2022-09-01 11:39:23 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+//Webpage that plays audio with HTML5 & CSS3
+
+function play1() {
+    let audio = new Audio("perfect.mp3");
+    audio.play();
+}
+function play2() {
+    let audio = new Audio("perfect 2.mp3");
+    audio.play();
+}
+function play3() {
+    let audio = new Audio("lollipop.mp3");
+    audio.play();
+}
