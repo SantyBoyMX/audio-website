d09d97a6029149d867ab9c85abf856ab013206c9
SantyBoyMX <97777706+SantyBoyMX@users.noreply.github.com>
2022-09-01 11:42:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //Webpage that plays audio with HTML5 & CSS3
 
 function play1() {
-    let audio = new Audio("perfect.mp3");
+    let audio = new Audio("perfect.mp3"); // Every mp3 audio can be changed to the file name
     audio.play();
 }
 function play2() {
-    let audio = new Audio("perfect 2.mp3");
+    let audio = new Audio("perfect 2.mp3"); // Just remember to put all the audio files in the same directory
     audio.play();
 }
 function play3() {
